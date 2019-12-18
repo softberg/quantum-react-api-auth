eacfd4482aa11f835b7f16f15256b6b804232cd8
camp4 <amirjanyanit@gmail.com>
2019-12-18 16:38:43 +0400

    initial setup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+import Home from './Components/Home/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/" component={Home} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
